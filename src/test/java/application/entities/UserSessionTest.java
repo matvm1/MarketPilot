175ded2ef61ec08002e1d1e0a7760acc1c0902a8
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-08-25 18:12:27 -0400

    test(UserSession): Created unit tests for UserSession. Created constant permission sets for testing.

--- src/test/java/application/entities/UserSessionTest.java
+++ src/test/java/application/entities/UserSessionTest.java
@@ -1,21 +1,46 @@
 package application.entities;
 
-import domain.entities.auth.User;
-import domain.entities.auth.UserRoleAssignment;
+import domain.entities.auth.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class UserSessionTest {
-    private User analystAndInvestor;
+    private User analystAndInvestorUser;
+    private UserSession investorSession;
+    private UserSession analystSession;
 
     @BeforeEach
     void setUp() {
-        Set<UserRoleAssignment> userRoles =
-        analystAndInvestor = new User(2, );
+        Set<UserRoleAssignment> userRoleAssignments = new HashSet<>();
+        userRoleAssignments.add(new UserRoleAssignment(new Role(Role.RoleName.PersonalInvestor, RolePermissionSets.PERSONAL_INVESTOR_PERMISSIONS)));
+        userRoleAssignments.add(new UserRoleAssignment(new Role(Role.RoleName.Analyst, RolePermissionSets.ANALYST_PERMISSIONS)));
+        analystAndInvestorUser = new User(2, userRoleAssignments, "John", "M", "Doe");
+        investorSession = new UserSession(analystAndInvestorUser, Role.RoleName.PersonalInvestor);
+        // TODO: session state management - can't have a personal and employee session running at once
+        investorSession = new UserSession(analystAndInvestorUser, Role.RoleName.Analyst);
     }
 
     @Test
     void constructorThrowsForNullUser() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new UserSession(null, Role.RoleName.PersonalInvestor));
+    }
 
+    @Test
+    void constructorThrowsForNullActiveRole() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new UserSession(analystAndInvestorUser, null));
+    }
+
+    @Test
+    void getEffectivePermissionsReturnsCorrectFlatSetOfPermissions() {
+        assertEquals(RolePermissionSets.PERSONAL_INVESTOR_PERMISSIONS, investorSession.getEffectivePermissions());
+        assertEquals(RolePermissionSets.ANALYST_PERMISSIONS, analystSession.getEffectivePermissions());
     }
 }
