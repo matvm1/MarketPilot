5d37ecf19f43a97e7c40c3028853793109a7c071
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-09-17 17:48:00 -0400

    fix(RoleRepository): Extended BaseRepository

--- src/main/java/com/marketpilot/application/ports/RoleRepository.java
+++ src/main/java/com/marketpilot/application/ports/RoleRepository.java
@@ -4,6 +4,6 @@ import com.marketpilot.domain.entities.auth.Role;
 
 import java.util.Optional;
 
-public interface RoleRepository {
+public interface RoleRepository extends BaseRepository<Role, Long> {
     Optional<Role> findByRoleName(Role.RoleName roleName);
 }
