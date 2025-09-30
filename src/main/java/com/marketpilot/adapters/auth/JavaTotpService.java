482769361ea9e076ba77d700500dab9336bdc8f9
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-09-29 21:16:34 -0400

    refactor(JavaTotpService): Injected TimeProvider so that TimeProivder mocks and stubs can be used in unit tests. Created unit tests

--- src/main/java/com/marketpilot/adapters/auth/JavaTotpService.java
+++ src/main/java/com/marketpilot/adapters/auth/JavaTotpService.java
@@ -4,11 +4,16 @@ import com.marketpilot.application.dto.auth.credentials.MfaCredential;
 import com.marketpilot.application.dto.auth.credentials.TotpCredential;
 import com.marketpilot.application.ports.auth.TwoFactorService;
 import dev.samstevens.totp.code.*;
-import dev.samstevens.totp.time.SystemTimeProvider;
 import dev.samstevens.totp.time.TimeProvider;
 
 // TOTP Verification by https://github.com/samdjstevens/java-totp
 public class JavaTotpService implements TwoFactorService {
+    private final TimeProvider timeProvider;
+
+    public JavaTotpService(TimeProvider timeProvider) {
+        this.timeProvider = timeProvider;
+    }
+
     //TODO: Unit tests
     @Override
     public boolean verify(MfaCredential credentials) {
@@ -18,7 +23,6 @@ public class JavaTotpService implements TwoFactorService {
         if (!(credentials instanceof TotpCredential))
             return false;
 
-        TimeProvider timeProvider = new SystemTimeProvider();
         CodeGenerator codeGenerator = new DefaultCodeGenerator(HashingAlgorithm.SHA512);
         CodeVerifier verifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
 
---
+++ src/test/java/com/marketpilot/adapters/persistence/auth/JavaTotpServiceTest.java
@@ -0,0 +1,82 @@
+package com.marketpilot.adapters.auth;
+
+import com.marketpilot.application.dto.auth.credentials.MfaCredential;
+import com.marketpilot.application.dto.auth.credentials.TotpCredential;
+import com.marketpilot.domain.entities.auth.Role.RoleName;
+import dev.samstevens.totp.code.*;
+import dev.samstevens.totp.exceptions.CodeGenerationException;
+import dev.samstevens.totp.time.SystemTimeProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import dev.samstevens.totp.time.TimeProvider;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class JavaTotpServiceTest {
+
+    private JavaTotpService javaTotpService;
+    @Mock private TimeProvider mockTimeProvider;
+
+    @BeforeEach
+    void setUp() {
+        javaTotpService = new JavaTotpService(mockTimeProvider);
+    }
+
+    @Test
+    void verify_returnsFalse_forNullCredential() {
+        assertFalse(javaTotpService.verify(null));
+    }
+
+    @Test
+    void verify_returnsFalse_forInvalidCredentialType() {
+        MfaCredential mockCredential = mock(MfaCredential.class);
+        assertFalse(javaTotpService.verify(mockCredential));
+    }
+
+    @Test
+    void verify_returnsFalse_forInvalidCode() {
+        String secret = "JBSWY3DPEHPK3PXP";
+        long fixedTime = 1_700_000_000_000L;
+        when(mockTimeProvider.getTime()).thenReturn(fixedTime);
+        TotpCredential credential = new TotpCredential(UUID.randomUUID(), RoleName.PersonalInvestor, secret, "012345");
+
+        assertFalse(javaTotpService.verify(credential));
+    }
+
+    @Test
+    void verify_returnsTrue_forValidCodeInFixedTime() {
+        String secret = "JBSWY3DPEHPK3PXP";
+        long fixedTime = 1_700_000_000_000L;
+        when(mockTimeProvider.getTime()).thenReturn(fixedTime);
+        TotpCredential credential = new TotpCredential(UUID.randomUUID(), RoleName.PersonalInvestor, secret, "813407");
+
+        assertTrue(javaTotpService.verify(credential));
+    }
+
+    @Test
+    void verify_returnsTrue_forValidCodeInCurrentTime() {
+        String secret = "JBSWY3DPEHPK3PXP";
+        long currentTime = System.currentTimeMillis();
+        int interval = 30;
+        when(mockTimeProvider.getTime()).thenReturn(currentTime);
+
+        CodeGenerator codeGenerator = new DefaultCodeGenerator(HashingAlgorithm.SHA512);
+        String validCode = null;
+        try {
+            validCode = codeGenerator.generate(secret, currentTime / interval);
+        } catch (CodeGenerationException e) {
+            throw new RuntimeException(e);
+        }
+
+        TotpCredential credential = new TotpCredential(UUID.randomUUID(), RoleName.PersonalInvestor, secret, validCode);
+
+        assertTrue(javaTotpService.verify(credential));
+    }
+}
