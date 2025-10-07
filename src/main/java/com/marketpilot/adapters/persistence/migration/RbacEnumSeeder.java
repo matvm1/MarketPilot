cf2fc8fc417031c16442eb3c22472f6773257099
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-10-07 13:32:37 -0400

    refactor(RbacEnumSeeder): Renamed EnumSeeder as RbacEnumSeeder

--- src/main/java/com/marketpilot/adapters/persistence/migration/RbacEnumSeeder.java
+++ src/main/java/com/marketpilot/adapters/persistence/migration/RbacEnumSeeder.java
@@ -2,8 +2,12 @@ package com.marketpilot.adapters.persistence.migration;
 
 import com.marketpilot.domain.entities.auth.Permission;
 import com.marketpilot.domain.entities.auth.Role.RoleName;
+import com.marketpilot.domain.entities.auth.UserType;
 
-public class EnumSeeder {
+// seeds database with enum values from enum classes pertaining to RBAC
+// enums serve as the source of truth for permission and rolename values
+// data in db serves as the source of truth for Role objects to allow configurability byb admins/ops
+public class RbacEnumSeeder {
     public static void main(String[] args) {
         EnumSeederUtil.seed(Permission.class, "APP_PERMISSION");
         EnumSeederUtil.seed(RoleName.class, "APP_ROLE");
