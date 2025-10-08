c3c5194bbba16b267aa5c70f9b71468e9e01f289
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-10-08 05:47:14 -0400

    feat(JdbcExecutor): Can now fetch result sets that are converted to a T record, to a Map<T, U> map, or a Map<ID, Set<T>> multi-map

--- src/main/java/com/marketpilot/adapters/persistence/jdbc/JdbcExecutor.java
+++ src/main/java/com/marketpilot/adapters/persistence/jdbc/JdbcExecutor.java
@@ -6,24 +6,122 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.*;
 
 // Do not expose outside MarketPilot project
 //TODO: validate incoming sql
+//TODO: unit tests, integration tests
 public class JdbcExecutor {
     private static final PoolDataSource pool = ConnectionPool.getPool();
 
     private JdbcExecutor() {}
 
-    // prepares a PreparedStatement, executes, and returns the queried data wrapped in a ResultSet
-    public static ResultSet executeQuery(String sql, Param... params) {
+    public static <T> Optional<T> fetchRecord(String sql, ResultSetToValue<T> resultSetToValue, Param... params) {
         try (Connection conn = pool.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             setParameters(ps, params);
-            return ps.executeQuery();
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return Optional.of(resultSetToValue.map(rs));
         }
         catch (SQLException e) {
             //TODO: Rollback? Commit?
             //TODO: Log
+            e.printStackTrace();
+            throw new RuntimeException("Could not execute query:\n" + sql);
+        }
+    }
+
+    // prepares a PreparedStatement, executes, and returns cached queried data in a List
+    /*public static <T> Optional<List<T>> executeQueryToList(String sql, ResultSetToValue<T> resultSetToValue, Param... params) {
+        try (Connection conn = pool.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            setParameters(ps, params);
+
+            ResultSet rs = ps.executeQuery();
+
+            List<T> dataCache = new ArrayList<>();
+            while (rs.next())
+                dataCache.add(resultSetToValue.map(rs));
+            return dataCache.isEmpty() ? Optional.empty() : Optional.of(dataCache);
+        }
+        catch (SQLException e) {
+            //TODO: Rollback? Commit?
+            //TODO: Log
+            e.printStackTrace();
+            throw new RuntimeException("Could not execute query:\n" + sql);
+        }
+    }*/
+
+    // prepares a PreparedStatement, executes, and returns cached queried data cached in a Tuple
+    /*public static <T, U> Optional<Tuple<T, U>> executeQueryToTuple(String sql,
+           ResultSetToValue<T> resultSetToValueT,
+           ResultSetToValue<U> resultSetToValueU,
+           Param... params) {
+        try (Connection conn = pool.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            setParameters(ps, params);
+
+            ResultSet rs = ps.executeQuery();
+
+            Tuple<T, U> dataCache = new Tuple<T, U>(resultSetToValueT.map(rs), resultSetToValueU.map(rs));
+            return Optional.of(dataCache);
+        }
+        catch (SQLException e) {
+            //TODO: Rollback? Commit?
+            //TODO: Log
+            e.printStackTrace();
+            throw new RuntimeException("Could not execute query:\n" + sql);
+        }
+    }*/
+
+    // prepares a PreparedStatement, executes, and returns cached queried data in a Map
+    public static <T, U> Optional<Map<T, U>> executeQueryToMap(String sql,
+       ResultSetToValue<T> resultSetToValueT,
+       ResultSetToValue<U> resultSetToValueU,
+       Param... params) {
+        try (Connection conn = pool.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            setParameters(ps, params);
+
+            ResultSet rs = ps.executeQuery();
+
+            Map<T, U> dataCache = new HashMap<>();
+            while (rs.next()) {
+                dataCache.put(resultSetToValueT.map(rs), resultSetToValueU.map(rs));
+            }
+            return dataCache.isEmpty() ? Optional.empty() : Optional.of(dataCache);
+        }
+        catch (SQLException e) {
+            //TODO: Rollback? Commit?
+            //TODO: Log
+            e.printStackTrace();
+            throw new RuntimeException("Could not execute query:\n" + sql);
+        }
+    }
+
+    // prepares a PreparedStatement, executes, and returns cached queried data in a multi-map
+    public static <ID, T> Optional<Map<ID, Set<T>>> executeQueryToMultiMap(
+            String sql,
+            ResultSetToValue<ID> keyMapper,
+            ResultSetToValue<T> valueMapper,
+            Param... params
+    ) {
+        try (Connection conn = pool.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            setParameters(ps, params);
+
+            ResultSet rs = ps.executeQuery();
+
+            Map<ID, Set<T>> dataCache = new HashMap<>();
+            while (rs.next()) {
+                ID key = keyMapper.map(rs);
+                T value = valueMapper.map(rs);
+
+                dataCache.computeIfAbsent(key, k -> new HashSet<>()).add(value);
+            }
+            return dataCache.isEmpty() ? Optional.empty() : Optional.of(dataCache);
+        } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException("Could not execute query:\n" + sql);
         }
