7126ea33b515de4f71a2fa960385cb30a52b151b
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-10-09 18:33:37 -0400

    feat(Tuple): Added static Tuple.of

--- src/main/java/com/marketpilot/util/Tuple.java
+++ src/main/java/com/marketpilot/util/Tuple.java
@@ -1,3 +1,7 @@
 package com.marketpilot.util;
 
-public record Tuple<T, U> (T t, U u) {}
+public record Tuple<T, U> (T t, U u) {
+    public static <T, U>  Tuple<T, U> of(T t, U u) {
+        return new Tuple<>(t, u);
+    }
+}
