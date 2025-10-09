247f011d93e822a42295e3cc7cdf4f8701175a68
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-10-09 17:20:21 -0400

    refactor(JavaTotpService): Now using SHA-256 based hashing with approriate byte sizes for secrets

--- src/test/java/com/marketpilot/adapters/auth/JavaTotpServiceTest.java
+++ src/test/java/com/marketpilot/adapters/auth/JavaTotpServiceTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,10 +23,12 @@ class JavaTotpServiceTest {
 
     private JavaTotpService javaTotpService;
     @Mock private TimeProvider mockTimeProvider;
+    private final char[] dummySecret = "CH4LOY3ZDPAI3ZXPRLZSWUQGF6ZYSJIPLTS6GGU7K4WJGK2O5VVA====".toCharArray();
+    private final HashingAlgorithm dummyHashingAlorithm = HashingAlgorithm.SHA256;
 
     @BeforeEach
     void setUp() {
-        javaTotpService = new JavaTotpService(mockTimeProvider, HashingAlgorithm.SHA256);
+        javaTotpService = new JavaTotpService(mockTimeProvider, dummyHashingAlorithm);
     }
 
     @Test
@@ -41,10 +44,10 @@ class JavaTotpServiceTest {
 
     @Test
     void verify_returnsFalse_forInvalidCode() {
-        String secret = "JBSWY3DPEHPK3PXP";
         long fixedTime = 1_700_000_000_000L;
         when(mockTimeProvider.getTime()).thenReturn(fixedTime);
         TotpCredential credential = new TotpCredential(UUID.randomUUID(), RoleName.PersonalInvestor,  "012345");
+        credential.setSecret(dummySecret);
 
         assertFalse(javaTotpService.verify(credential));
     }
@@ -52,30 +55,30 @@ class JavaTotpServiceTest {
     //TODO: Test with 64-byte secret
     @Test
     void verify_returnsTrue_forValidCodeInFixedTime() {
-        String secret = "JBSWY3DPEHPK3PXP";
         long fixedTime = 1_700_000_000_000L;
         when(mockTimeProvider.getTime()).thenReturn(fixedTime);
-        TotpCredential credential = new TotpCredential(UUID.randomUUID(), RoleName.PersonalInvestor, "813407");
+        TotpCredential credential = new TotpCredential(UUID.randomUUID(), RoleName.PersonalInvestor, "416039");
+        credential.setSecret(dummySecret);
 
         assertTrue(javaTotpService.verify(credential));
     }
 
     @Test
     void verify_returnsTrue_forValidCodeInCurrentTime() {
-        String secret = "JBSWY3DPEHPK3PXP";
         long currentTime = System.currentTimeMillis();
         int interval = 30;
         when(mockTimeProvider.getTime()).thenReturn(currentTime);
 
-        CodeGenerator codeGenerator = new DefaultCodeGenerator(HashingAlgorithm.SHA512);
+        CodeGenerator codeGenerator = new DefaultCodeGenerator(dummyHashingAlorithm);
         String validCode = null;
         try {
-            validCode = codeGenerator.generate(secret, currentTime / interval);
+            validCode = codeGenerator.generate(Arrays.toString(dummySecret), currentTime / interval);
         } catch (CodeGenerationException e) {
             throw new RuntimeException(e);
         }
 
         TotpCredential credential = new TotpCredential(UUID.randomUUID(), RoleName.PersonalInvestor, validCode);
+        credential.setSecret(dummySecret);
 
         assertTrue(javaTotpService.verify(credential));
     }
