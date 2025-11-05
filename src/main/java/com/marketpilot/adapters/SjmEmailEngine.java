8da667efc2762333cb2cbec5b2d039ef061cc5d7
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-11-04 19:52:05 -0500

    feat(SjmEmailEngine): Can now get single instance of SjmEmailEngine to send text-body emails

---
+++ src/main/java/com/marketpilot/adapters/SjmEmailEngine.java
@@ -0,0 +1,76 @@
+package com.marketpilot.adapters;
+
+import com.marketpilot.application.dto.EmailMessage;
+import com.marketpilot.application.ports.EmailEngine;
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+// Simple Java Mail
+public class SjmEmailEngine implements EmailEngine {
+    private static SjmEmailEngine instance;
+    private static Mailer mailer;
+    private static String smtpHost;
+    private static String smtpEmail;
+    private static String smtpPassword;
+
+    private SjmEmailEngine() {
+        readCredentials();
+
+        mailer = MailerBuilder
+                .withSMTPServer(smtpHost, 587, smtpEmail, smtpPassword)
+                .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                .buildMailer();
+    }
+
+    public static SjmEmailEngine getInstance() {
+        if (instance == null)
+            instance = new SjmEmailEngine();
+
+        return instance;
+    }
+
+    @Override
+    public boolean sendEmail(EmailMessage message) {
+        Email email = EmailBuilder.startingBlank()
+                .from(smtpEmail)
+                .to(message.recipient())
+                .withSubject(message.subject())
+                .withPlainText(message.body())
+                .buildEmail();
+
+        mailer.sendMail(email);
+        return true;
+    }
+
+    @Override
+    public boolean sendTemplatedEmail(EmailMessage message, String templateFileName) {
+        return false;
+    }
+
+    private static void readCredentials() {
+        String propsPath = System.getenv("SMTP_PROPERTIES_PATH");
+
+        if (propsPath == null) {
+            throw new IllegalArgumentException("SMTP_PROPERTIES_PATH environment variable is not set");
+        }
+
+        Properties props = new Properties();
+        try(FileInputStream fis = new FileInputStream(propsPath)) {
+            props.load(fis);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        smtpHost = props.getProperty("SMTP_HOST");
+        smtpEmail = props.getProperty("SMTP_EMAIL");
+        smtpPassword = props.getProperty("SMTP_PASSWORD");
+    }
+}
