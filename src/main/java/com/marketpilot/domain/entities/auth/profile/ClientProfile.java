822b19a82f8826971c55182823352c2ab99d8204
matvm1 <15144366+matvm1@users.noreply.github.com>
2026-03-04 18:29:25 -0500

    feat(User Profiles): Modeled user client/employee data as separate profiles with fields and validators present in User

---
+++ src/main/java/com/marketpilot/domain/entities/auth/profile/ClientProfile.java
@@ -0,0 +1,44 @@
+package com.marketpilot.domain.entities.auth.profile;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+public class ClientProfile {
+    private String email;
+
+    public ClientProfile(String email) {
+        if (email == null)
+            throw new IllegalArgumentException("email cannot be null");
+        if (email.isBlank())
+            throw new IllegalArgumentException("email cannot be blank");
+        EmailValidator emailValidator = EmailValidator.getInstance();
+        if (!emailValidator.isValid(email))
+            throw new IllegalArgumentException("email '" + email + "' is not in a valid email format");
+
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        if (email == null)
+            throw new IllegalArgumentException("email cannot be null");
+        if (email.isBlank())
+            throw new IllegalArgumentException("email cannot be blank");
+
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (!(o instanceof ClientProfile))
+            return false;
+
+        return this.email.equals(((ClientProfile) o).email);
+    }
+}
