7ad71aeba7bd5516fb89c19cded97900674e1797
matvm1 <15144366+matvm1@users.noreply.github.com>
2026-03-05 22:58:01 -0500

    fix(PersistentEntity): Made no-args constructor protected

--- src/main/java/com/marketpilot/domain/entities/PersistentEntity.java
+++ src/main/java/com/marketpilot/domain/entities/PersistentEntity.java
@@ -11,7 +11,7 @@ public abstract class PersistentEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public PersistentEntity() {}
+    protected PersistentEntity() {}
 
     public Long getId() { return id; }
 }
