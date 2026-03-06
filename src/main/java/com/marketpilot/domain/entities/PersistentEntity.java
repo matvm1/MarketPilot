81c7f74ba5b4ba2e5b7ca0a8535a1762c7da2ad9
matvm1 <15144366+matvm1@users.noreply.github.com>
2026-03-05 23:02:12 -0500

    feat(PersistentEntity): Added equals() override

--- src/main/java/com/marketpilot/domain/entities/PersistentEntity.java
+++ src/main/java/com/marketpilot/domain/entities/PersistentEntity.java
@@ -14,4 +14,15 @@ public abstract class PersistentEntity {
     protected PersistentEntity() {}
 
     public Long getId() { return id; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (!(o instanceof PersistentEntity that))
+            return false;
+
+        return id != null && id.equals(that.id);
+    }
 }
