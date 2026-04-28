e73e74caabbba47e2474af94d08366ce671fe3ae
matvm1 <15144366+matvm1@users.noreply.github.com>
2026-04-27 23:47:57 -0400

    refactor(extendSchema): Auth fields will be migrated to their own table.

--- src/main/java/com/marketpilot/adapters/persistence/migration/PostConstructDdl.java
+++ src/main/java/com/marketpilot/adapters/persistence/migration/PostConstructDdl.java
@@ -15,13 +15,21 @@ public class PostConstructDdl {
     @PostConstruct
     public void extendSchema() {
         System.out.println("Extending schema");
-        jdbcClient.sql("ALTER TABLE APP_USER ADD COLUMN IF NOT EXISTS CLIENT_REGISTRATION_CODE VARCHAR2(16)").update();
-        jdbcClient.sql("ALTER TABLE APP_USER ADD COLUMN IF NOT EXISTS CLIENT_REGISTRATION_EXPIRATION TIMESTAMP").update();
-        jdbcClient.sql("ALTER TABLE APP_USER ADD COLUMN IF NOT EXISTS EMPLOYEE_REGISTRATION_CODE VARCHAR2(16)").update();
-        jdbcClient.sql("ALTER TABLE APP_USER ADD COLUMN IF NOT EXISTS EMPLOYEE_REGISTRATION_EXPIRATION TIMESTAMP").update();
-        jdbcClient.sql("ALTER TABLE APP_USER ADD COLUMN IF NOT EXISTS CLIENT_TOTP_SECRET VARCHAR2(64 CHAR)").update();
-        jdbcClient.sql("ALTER TABLE APP_USER ADD COLUMN IF NOT EXISTS EMPLOYEE_TOTP_SECRET VARCHAR2(64 CHAR)").update();
-        jdbcClient.sql("ALTER TABLE APP_USER ADD COLUMN IF NOT EXISTS CLIENT_USER_STATUS_ID NUMBER(2)").update();
-        jdbcClient.sql("ALTER TABLE APP_USER ADD COLUMN IF NOT EXISTS EMPLOYEE_USER_STATUS_ID NUMBER(2)").update();
+
+        jdbcClient.sql("""
+                CREATE TABLE APP_USER_AUTH (
+                    USER_ID        NUMBER PRIMARY KEY,
+                    CLIENT_REGISTRATION_CODE       VARCHAR2(16),
+                    CLIENT_REGISTRATION_EXPIRATION TIMESTAMP,
+                    CLIENT_TOTP_SECRET             VARCHAR2(64 CHAR),
+                    CLIENT_USER_STATUS_ID          NUMBER(2),
+                    EMPLOYEE_REGISTRATION_CODE       VARCHAR2(16),
+                    EMPLOYEE_REGISTRATION_EXPIRATION TIMESTAMP,
+                    EMPLOYEE_TOTP_SECRET             VARCHAR2(64 CHAR),
+                    EMPLOYEE_USER_STATUS_ID          NUMBER(2),
+                
+                    CONSTRAINT FK_APP_USER_AUTH FOREIGN KEY (USER_ID) REFERENCES APP_USER(ID)
+                );
+                """).update();
     }
 }
