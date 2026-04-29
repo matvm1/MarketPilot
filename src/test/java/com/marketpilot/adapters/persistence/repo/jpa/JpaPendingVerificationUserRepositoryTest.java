31fe68a234e5353a8e700927636c0a320f90ad21
matvm1 <15144366+matvm1@users.noreply.github.com>
2026-04-29 12:35:57 -0400

    refactor(JpaPendingVerificationUserRepository): Moved DDL script to src/test/resources/sql and added @Sql annotation to run the script outside a transactional & non-static context (so it only runs once for all tests)

--- src/test/java/com/marketpilot/adapters/persistence/repo/jpa/JpaPendingVerificationUserRepositoryTest.java
+++ src/test/java/com/marketpilot/adapters/persistence/repo/jpa/JpaPendingVerificationUserRepositoryTest.java
@@ -6,17 +6,16 @@ import com.marketpilot.domain.entities.auth.*;
 import com.marketpilot.domain.repo.PendingVerificationUserRepository;
 import com.marketpilot.domain.repo.RoleRepository;
 import com.marketpilot.util.BufferedConverter;
-import config.AppConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.sql.SQLException;
 import java.util.HashSet;
@@ -30,6 +29,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 @Import({JpaPendingVerificationUserRepository.class, JpaRoleRepository.class})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(scripts = "/sql/schema-ddl.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 public class JpaPendingVerificationUserRepositoryTest {
     @Autowired private JdbcClient jdbcClient;
     @Autowired private PendingVerificationUserRepository pendingVerificationUserRepository;
@@ -38,37 +38,18 @@ public class JpaPendingVerificationUserRepositoryTest {
     private UserFactory userFactory;
     private User clientUser;
     private Set<Role> clientRoles;
+    private Set<Role> employeeRoles;
 
     private final byte[] dummyPasswordHash = BufferedConverter.toBytes("xcusdhfgvasj@#njkhbf@nmdsejkhf%jnkjkbhjsd!!@$%bn1sdasd2n19xvds71ns3");
 
-    private String schemaDdl = """
-                CREATE TABLE APP_USER_AUTH (
-                    USER_ID        NUMBER PRIMARY KEY,
-                    IS_CLIENT    BOOLEAN DEFAULT FALSE NOT NULL,
-                    CLIENT_PASSWORD_HASH RAW(200) NULL,
-                    CLIENT_REGISTRATION_CODE       VARCHAR2(16),
-                    CLIENT_REGISTRATION_EXPIRATION TIMESTAMP NULL,
-                    CLIENT_TOTP_SECRET             VARCHAR2(64),
-                    CLIENT_USER_STATUS_ID          NUMBER(2),
-                    IS_EMPLOYEE  BOOLEAN DEFAULT FALSE NOT NULL,
-                    EMPLOYEE_PASSWORD_HASH RAW(200) NULL,
-                    EMPLOYEE_REGISTRATION_CODE       VARCHAR2(16),
-                    EMPLOYEE_REGISTRATION_EXPIRATION TIMESTAMP NULL,
-                    EMPLOYEE_TOTP_SECRET             VARCHAR2(64),
-                    EMPLOYEE_USER_STATUS_ID          NUMBER(2),
-                
-                    CONSTRAINT FK_APP_USER_AUTH FOREIGN KEY (USER_ID) REFERENCES APP_USER(ID)
-                );
-                """;
-
     @BeforeEach
     public void setUp() {
-        jdbcClient.sql(schemaDdl).update();
-
         roleRepository.save(TestRoles.PERSONAL_INVESTOR_ROLE);
 
         clientRoles = new HashSet<>();
         clientRoles.add(TestRoles.PERSONAL_INVESTOR_ROLE);
+        employeeRoles = new HashSet<>();
+        employeeRoles.add(TestRoles.ANALYST_ROLE);
 
         userFactory = new UserFactory();
         clientUser = userFactory.createClientUser(clientRoles, "johnmdoe", "johnmdoe@outlook.com",
@@ -89,5 +70,11 @@ public class JpaPendingVerificationUserRepositoryTest {
                 .query(Integer.class)
                 .single();
         assertEquals(clientRoles.size(), clientUserRoleCount);
+    }
+
+    @Test
+    public void crossRegister_persistsUser() throws SQLException {
+        pendingVerificationUserRepository.registerNewUser(UserType.CLIENT, clientUser, clientRoles, dummyPasswordHash, "abc123");
+        pendingVerificationUserRepository.crossRegister(UserType.EMPLOYEE, clientUser, employeeRoles, dummyPasswordHash, "321cba");
     }
 }
