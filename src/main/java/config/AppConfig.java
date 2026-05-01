ee926a123ae9adbd78228c739f0deb8ccc3cfbae
matvm1 <15144366+matvm1@users.noreply.github.com>
2026-05-01 00:15:42 -0400

    chore(AppConfig): Added repo component scanning

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -1,10 +1,12 @@
 package config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @Import({AuthServiceConfig.class,
         RegistrationServiceConfig.class,
         DataAccessConfig.class})
+@ComponentScan(basePackages = "com.marketpilot.adapters.persistence.repo")
 public class AppConfig {
 
 }
