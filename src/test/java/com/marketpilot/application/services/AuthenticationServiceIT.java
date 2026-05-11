40b31db1792c2ee35370b31de7d85708cf4a4b07
matvm1 <15144366+matvm1@users.noreply.github.com>
2026-05-11 17:52:46 -0400

    test(AuthenticationService): Integration test for initiateEmployeeAuthentication()

--- src/test/java/com/marketpilot/application/services/AuthenticationServiceIT.java
+++ src/test/java/com/marketpilot/application/services/AuthenticationServiceIT.java
@@ -20,6 +20,7 @@ public class AuthenticationServiceIT extends RegistrationFixtureIT {
     @Autowired private AuthenticationService authenticationService;
 
     private static final String clientEmail = "quinnjordan@personal.com";
+    private static final String employeeId = "ab123456";
     private static final String employeeEmail = "quinnjordan@company.com";
 
     @Test
@@ -30,6 +31,15 @@ public class AuthenticationServiceIT extends RegistrationFixtureIT {
         assertTrue(result.u().isPresent());
         assertEquals(clientUser, result.u().get().principal());
         assertEquals(TestRoles.personalInvestorRole(), result.u().get().role());
-        //jdbc
+    }
+
+    @Test
+    public void initiateEmployeeAuthentication_succeedsForValidCredentials() {
+        Tuple<AuthenticationService.AuthenticationStatus, Optional<AuthenticationContext>> result =
+                authenticationService.initiateEmployeeAuthentication(employeeId, TestAuthProperties.dummyPassword(), Role.RoleName.Analyst);
+        assertEquals(AuthenticationService.AuthenticationStatus.AWAITING_2FA, result.t());
+        assertTrue(result.u().isPresent());
+        assertEquals(employeeUser, result.u().get().principal());
+        assertEquals(TestRoles.analystRole(), result.u().get().role());
     }
 }
