337a653b64204baecabdf7a0d9dbbacb5541bc40
matvm1 <15144366+matvm1@users.noreply.github.com>
2026-05-11 23:29:05 -0400

    fix(schema-ddl.sql, PostConstructDdl): Changed TOTP secrets type to RAW, aligned syntax formatting

--- src/main/java/com/marketpilot/adapters/persistence/migration/PostConstructDdl.java
+++ src/main/java/com/marketpilot/adapters/persistence/migration/PostConstructDdl.java
@@ -17,24 +17,23 @@ public class PostConstructDdl {
 
         jdbcClient.sql("""
                 CREATE TABLE APP_USER_AUTH (
-                    USER_ID        NUMBER PRIMARY KEY,
-                    UUID         RAW(16) NOT NULL,
+                    USER_ID                          NUMBER PRIMARY KEY,
+                    UUID                             RAW(16) NOT NULL,
                     MFATYPE_ID                       NUMBER(2),
-                    IS_CLIENT    BOOLEAN DEFAULT FALSE NOT NULL,
-                    CLIENT_PASSWORD_HASH RAW(200) NULL,
-                    CLIENT_REGISTRATION_CODE       VARCHAR2(16),
-                    CLIENT_REGISTRATION_EXPIRATION TIMESTAMP DEFAULT (SYSTIMESTAMP + INTERVAL '30' MINUTE) NULL,
-                    CLIENT_TOTP_SECRET             VARCHAR2(64 CHAR),
-                    CLIENT_USER_STATUS_ID          NUMBER(2),
-                    IS_EMPLOYEE  BOOLEAN DEFAULT FALSE NOT NULL,
-                    EMPLOYEE_PASSWORD_HASH RAW(200) NULL,
+                    IS_CLIENT                        BOOLEAN DEFAULT FALSE NOT NULL,
+                    CLIENT_PASSWORD_HASH             RAW(200) NULL,
+                    CLIENT_REGISTRATION_CODE         VARCHAR2(16),
+                    CLIENT_REGISTRATION_EXPIRATION   TIMESTAMP DEFAULT (SYSTIMESTAMP + INTERVAL '30' MINUTE) NULL,
+                    CLIENT_TOTP_SECRET               RAW(200) NULL,
+                    CLIENT_USER_STATUS_ID            NUMBER(2),
+                    IS_EMPLOYEE                      BOOLEAN DEFAULT FALSE NOT NULL,
+                    EMPLOYEE_PASSWORD_HASH           RAW(200) NULL,
                     EMPLOYEE_REGISTRATION_CODE       VARCHAR2(16),
                     EMPLOYEE_REGISTRATION_EXPIRATION TIMESTAMP DEFAULT (SYSTIMESTAMP + INTERVAL '30' MINUTE) NULL,
-                    EMPLOYEE_TOTP_SECRET             VARCHAR2(64 CHAR),
+                    EMPLOYEE_TOTP_SECRET             RAW(200) NULL,
                     EMPLOYEE_USER_STATUS_ID          NUMBER(2),
                 
                     CONSTRAINT FK_APP_USER_AUTH FOREIGN KEY (USER_ID) REFERENCES APP_USER(ID)
-                );
-                """).update();
+                );""").update();
     }
 }
