8784f5154649bd0c59a3e1043eb7e8ead0ad8716
matvm1 <15144366+matvm1@users.noreply.github.com>
2026-05-14 23:52:27 -0400

    refactor(JpaRoleRepositoryTest): Added replace = NONE to prevent H2 replacement, extended OracleTestBase to connect to oracle-free testcontainer

--- src/test/java/com/marketpilot/adapters/persistence/repo/jpa/JpaRoleRepositoryTest.java
+++ src/test/java/com/marketpilot/adapters/persistence/repo/jpa/JpaRoleRepositoryTest.java
@@ -3,18 +3,22 @@ package com.marketpilot.adapters.persistence.repo.jpa;
 import com.marketpilot.domain.entities.auth.Permission;
 import com.marketpilot.domain.entities.auth.Role;
 import com.marketpilot.domain.entities.auth.UserType;
+import integration.OracleTestBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
 
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase.Replace.NONE;
 
 @DataJpaTest
 @Import(JpaRoleRepository.class)
-class JpaRoleRepositoryTest {
+@AutoConfigureTestDatabase(replace = NONE)
+class JpaRoleRepositoryTest extends OracleTestBase {
 
     @Autowired
     private JpaRoleRepository roleRepository;
