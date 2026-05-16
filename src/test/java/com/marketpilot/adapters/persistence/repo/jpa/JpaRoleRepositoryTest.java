0f3363545f5393a5e9c32df7219ee0c816881c13
matvm1 <15144366+matvm1@users.noreply.github.com>
2026-05-16 02:14:57 -0400

    refactor(JpaRoleRepositoryTest): Now obtains an oracle test container with @OracleDataJpaTest

--- src/test/java/com/marketpilot/adapters/persistence/repo/jpa/JpaRoleRepositoryTest.java
+++ src/test/java/com/marketpilot/adapters/persistence/repo/jpa/JpaRoleRepositoryTest.java
@@ -3,22 +3,18 @@ package com.marketpilot.adapters.persistence.repo.jpa;
 import com.marketpilot.domain.entities.auth.Permission;
 import com.marketpilot.domain.entities.auth.Role;
 import com.marketpilot.domain.entities.auth.UserType;
-import integration.OracleTestBase;
+import integration.infrastructure.OracleDataJpaTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
 
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase.Replace.NONE;
 
-@DataJpaTest
+@OracleDataJpaTest
 @Import(JpaRoleRepository.class)
-@AutoConfigureTestDatabase(replace = NONE)
-class JpaRoleRepositoryTest extends OracleTestBase {
+class JpaRoleRepositoryTest {
 
     @Autowired
     private JpaRoleRepository roleRepository;
